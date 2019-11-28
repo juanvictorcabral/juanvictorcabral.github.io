b1309a1be266031db773f912b904e3b2d2a99d6f
Juan Cabral <juancabral.rt@gmail.com>
2019-11-28 00:05:28 -0300

    launch

--- assets/php/mailer.php
+++ assets/php/mailer.php
@@ -1,24 +1,23 @@
 <?php
 if($_POST)
 {
-	$to_email = "youremail@example.com"; //Recipient email, Replace with own email here
+	$to_email = "juan@upprooms.com"; //Recipient email, Replace with own email here
 	$subject  = "An email from my website contact form";
-	
+
 	//check if its an ajax request, exit if not
 	if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-		
+
 		$output = json_encode(array( //create JSON data
-			'type'=>'error', 
+			'type'=>'error',
 			'text' => 'Sorry Request must be Ajax POST'
 		));
 		die($output); //exit script outputting json data
-	} 
-	
+	}
+
 	//Sanitize input data using PHP filter_var().
 	$user_name		= filter_var($_POST["name"], FILTER_SANITIZE_STRING);
 	$user_email		= filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-	$message		= filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-	
+
 	//additional php validation
 	if(strlen($user_name) < 2){ // If length is less than 4 it will output JSON error.
 		$output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
@@ -32,17 +31,17 @@ if($_POST)
 	// 	$output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
 	// 	die($output);
 	// }
-	
+
 	//email body
 	$message_body = $message."\r\n\r\n-".$user_name."\r\nEmail: ".$user_email."\r\n" ;
-	
+
 	//proceed with PHP email.
 	$headers = 'From: '. $user_email .'' . "\r\n" .
 	'Reply-To: '.$user_email.'' . "\r\n" .
 	'X-Mailer: PHP/' . phpversion();
-	
+
 	$send_mail = mail($to_email, $subject, $message_body, $headers);
-	
+
 	if(!$send_mail)
 	{
 		//If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
